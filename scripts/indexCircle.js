696a311f2bcaf9cb898baa60f12f10e8c3ada0b4
alizardguy <42082829+alizardguy@users.noreply.github.com>
2023-03-09 13:34:33 -0800

    Update indexCircle.js

--- scripts/indexCircle.js
+++ scripts/indexCircle.js
@@ -1,5 +1,11 @@
 var circle = document.getElementById('circle');
 document.addEventListener('mousemove', function(e) {
+    if (circle.style.visibility = 'visible'){
     circle.style.top = e.pageY + 'px';
     circle.style.left = e.pageX + 'px';
+    }
+    else{
+    circle.style.top = 0;
+    circle.style.left = 0;
+    }
 });
