4ee59cc5e5456b0b59c6f466aca74583ead62654
alizardguy <42082829+alizardguy@users.noreply.github.com>
2023-03-09 11:36:19 -0800

    Update customElements.js

--- scripts/customElements.js
+++ scripts/customElements.js
@@ -10,7 +10,7 @@ class MyHeader extends HTMLElement{
                 </div>
 
                 <nav class="navbar noSelect">
-                    <button type="button" onclick="location.href='https://alizardguy.github.io/index.html'">HOME</button>
+                    <button type="button" onclick="location.href='/index.html'">HOME</button>
                 </nav>
             </div>
         </header>
